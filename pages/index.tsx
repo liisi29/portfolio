571f5b569ea91bd91bb526ca930d1090fa7028af
liisi.raidaru <liisi.raidaru@turnit.com>
2022-07-30 04:37:20 +0300

    paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import SkillCloud from "../components/elements/cloud/SkillCloud";
-import EduCloud from "../components/elements/cloud/EduCloud";
-import Work from "../components/work/Work";
-import About from "../components/about/About";
-import Main from "../components/main/Main";
-import NavBar from "../components/navbar/Navbar";
-import Contact from "../components/contact/Contact";
-import Footer from "../components/footer/Footer";
+import SkillCloud from "@components/elements/cloud/SkillCloud";
+import EduCloud from "@components/elements/cloud/EduCloud";
+import Work from "@components/work/Work";
+import About from "@components/about/About";
+import Main from "@components/main/Main";
+import NavBar from "@components/navbar/Navbar";
+import Contact from "@components/contact/Contact";
+import Footer from "@components/footer/Footer";
 
 export default function Home() {
   return (
